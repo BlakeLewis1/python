2272641652344589349ea956b7df1f78da49a8dd
BlakeLewis1 <BLewis@academytrainee.com>
2020-05-01 16:51:37 +0100

    a bit late

---
+++ QA python/grade calculator.py
@@ -0,0 +1,19 @@
+#learning python
+#grade calculator code 
+
+physics =int(input("enter your physics mark"))
+chemistry =int(input("enter your chemistry mark"))
+maths =int(input("enter your maths mark")) 
+total = (physics + chemistry + maths)
+percentage =(total/300 * 100)
+print ("you got", percentage)
+if percentage < 40:
+    print("try again")
+if percentage > 40:
+    print("you got a D")
+if percentage > 50:
+    print("you got a c")
+if percentage > 60:
+    print("you got a b")
+if percentage > 70:
+    print ("you got an a, well done!")
