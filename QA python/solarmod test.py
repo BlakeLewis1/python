877a6d6e314871eb69954e2a7ed68f0a6c373415
BlakeLewis1 <BLewis@academytrainee.com>
2020-05-04 14:51:07 +0100

    modules task

---
+++ QA python/solarmod test.py
@@ -0,0 +1,3 @@
+import solarmod
+
+print (solarmod.planet("Earth"))
---
+++ QA python/solarmod.py
@@ -0,0 +1,8 @@
+def solar(name):
+    print(name + " Our solar system has 8 planets")
+
+def planet(name):
+    print(name + " this is our home planet")
+
+def star(name):
+    print(name + " this is a star")
