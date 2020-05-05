1d9627837e7d39dc2dadff3ed1938f4092c6bc13
BlakeLewis1 <BLewis@academytrainee.com>
2020-05-05 18:34:27 +0100

    added challenge

---
+++ QA python/challenge 05-05-20.py
@@ -0,0 +1,17 @@
+#challenge 05-05-20 
+import math
+num=input("enter your number")
+factorial = (math.factorial(int(num)))
+print("the factorial of ", num, "is: " , factorial)
+#print(math.factorial(int(num)))
+
+
+
+    #factor=(num*i)
+    #print(i)
+    #print(factor)
+    #print(num+factor*i)
+   
+
+    
+    
